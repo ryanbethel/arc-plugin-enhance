ed25dadea80a84ffd85847e9532732d6e70d3ae8
Ryan Bethel <ryan.bethel@gmail.com>
2023-12-11 13:56:55 -0500

    misc

---
+++ public/app/project-map.mjs
@@ -0,0 +1,8 @@
+export const projectMap = {
+  'app/api/index.mjs': 'http://localhost:3333/_public/app/api/index.mjs',
+  'app/pages/index.html': 'http://localhost:3333/_public/app/pages/index.html',
+  'app/api/about.mjs': 'http://localhost:3333/_public/app/api/about.mjs',
+  'app/pages/about.mjs': 'http://localhost:3333/_public/app/pages/about.mjs',
+  'app/elements/el-footer.mjs': 'http://localhost:3333/_public/app/elements/el-footer.mjs',
+  'app/elements/el-header.mjs': 'http://localhost:3333/_public/app/elements/el-header.mjs',
+}
